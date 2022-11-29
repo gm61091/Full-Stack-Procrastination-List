4919c93cc4f2fa8ec71494f8c0ef4f15eb3ad79a
gary merriman <g.merriman691@gmail.com>
2022-11-29 11:46:03 -0600

    save

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Full-Stack-Procrastination-List
+psql --host=ec2-3-217-251-77.compute-1.amazonaws.com --port=5432 --username=xeospjhfdnisgu --password --dbname=d1lqsag75fjjrj
--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const Pool = require("pg").Pool;
 const path = require('path')
 
 const pool = new Pool({
-    user: "postgres",
-    password: "Tarpon3473!",
-    host: "localhost",
+    user: "xeospjhfdnisgu",
+    password: "19b349e131e8e1e10defe600f970229901f1a58279fdbc5a4d20ffe1de8490d5",
+    host: "ec2-3-217-251-77.compute-1.amazonaws.com",
     port: "5432",
-    database: "todolist"
+    database: "d1lqsag75fjjrj"
 });
 
 module.exports = pool;
