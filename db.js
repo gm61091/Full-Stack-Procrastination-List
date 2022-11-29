787595fb441fb20f5c6d1c160174bb875e9deacd
gary merriman <g.merriman691@gmail.com>
2022-11-29 12:24:03 -0600

    site deply

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const Pool = require("pg").Pool;
 const path = require('path')
 
 const pool = new Pool({
-    user: "xeospjhfdnisgu",
-    password: "19b349e131e8e1e10defe600f970229901f1a58279fdbc5a4d20ffe1de8490d5",
-    host: "ec2-3-217-251-77.compute-1.amazonaws.com",
+    user: "postgres",
+    password: "Tarpon3473!",
+    host: "localhost",
     port: "5432",
-    database: "d1lqsag75fjjrj"
+    database: "todolist"
 });
 
 module.exports = pool;
